34b62e78386e3d724e9a56cda74cdcedbbf93784
Thamizhiniyan P <thamizhiniyan2907@gmail.com>
2021-11-08 03:11:24 +0000

    Convert csv files to dataframes

---
+++ munge/readcsv.R
@@ -0,0 +1,17 @@
+#read all csv file to data frame
+
+data_files = list.files("data/", pattern = "^c")  # Identify file names
+
+data_files # Print file names
+
+for(i in 1:length(data_files)) { #for loop to convert csv to data frames
+  
+  readcsvfile = read.csv(paste0("data/", data_files[i]))
+  
+  substringdata = substr(data_files[i], 1, nchar(data_files[i])-4)
+  
+  assign(substringdata, readcsvfile)
+  
+}
+
+
